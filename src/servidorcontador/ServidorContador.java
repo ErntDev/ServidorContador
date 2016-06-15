7a2ac7d003c97d7c35fb2846107d22b0a027970d
Ernesto GM <ernt26dev@gmail.com>
2016-06-14 23:24:50 -0500

    Añade vocales mayúsculas

--- src/servidorcontador/ServidorContador.java
+++ src/servidorcontador/ServidorContador.java
@@ -32,9 +32,11 @@ public class ServidorContador {
             int numConsonantes=0;
             
             for (int i = 0; i < cadena.length(); i++) {
-                if (cadena.charAt(i)>='a' && cadena.charAt(i)<='z') {
+                if (cadena.charAt(i)>='a' && cadena.charAt(i)<='z' || 
+                        cadena.charAt(i)>='A' && cadena.charAt(i)<='Z') {
                     switch (cadena.charAt(i)){
-                        case 'a': case 'e': case 'i': case 'o': case 'u':
+                        case 'a': case 'e': case 'i': case 'o': case 'u': 
+                        case'A': case 'E': case'I': case 'O': case 'U':
                             numVocales++;
                             break;
                         default:
